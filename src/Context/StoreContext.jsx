c123de39d47b175431c88026ef957b33aadd5abe
toumisana <toumisana230@gmail.com>
2024-06-03 01:02:14 +0100

    oook

--- src/Context/StoreContext.jsx
+++ src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://backend-1ixllswzj-toumisanas-projects.vercel.app/"
     // const [food_list, setFoodList] = useState([]);
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
